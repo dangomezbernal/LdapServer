b82e4e417e3560920b0870b4f959f7a56243a059
Dan Gómez <dangomezbernal@gmail.com>
2022-10-23 16:11:03 +0000

    Delete startup.sh

--- ldap22_practica/startup.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-
-rm -rf /var/lib/ldap/*
-rm -rf /etc/ldap/slapd.d/*
-slaptest -f slapd.conf -F /etc/ldap/slapd.d
-slapadd -F /etc/ldap/slapd.d -l edt-org.ldif
-chown openldap.openldap /etc/ldap/slapd.d/ /var/lib/ldap/
-/usr/sbin/slapd -d0
